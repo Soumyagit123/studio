cbc6e5a0247b979f2b40214f6e79c4cdc4a2dcb7
Soumya Dash <soumyaranjan.dash4@gmail.com>
2026-01-30 06:54:55 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2152605947-7d700",
+  "appId": "1:505858140774:web:8cf02f3d4c9ccf52cfdba7",
+  "apiKey": "AIzaSyAqgu5NbFPGoBCcbvG43Oaaw_JK5l1HkZ0",
+  "authDomain": "studio-2152605947-7d700.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "505858140774"
+};
